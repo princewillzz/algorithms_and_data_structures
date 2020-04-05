414f9b1ff56ad815cda20fe096636d049dcab061
princewillzz <princewillz2013@gmail.com>
2020-04-06 02:11:44 +0530

    test file added

---
+++ leetcode/test.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test {
+    public static void main(String argv[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+    }
+    
+    static int solve() {
+
+        return 0;
+    }
+
+}
