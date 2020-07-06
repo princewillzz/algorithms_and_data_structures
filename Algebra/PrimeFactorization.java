b91dfed771b488450861d20d5416f3997aeef69f
princewillzz <princewillz2013@gmail.com>
2020-07-06 20:17:29 +0530

    init commit

--- Algebra/PrimeFactorization.java
+++ Algebra/PrimeFactorization.java
@@ -6,17 +6,36 @@ public class PrimeFactorization {
     public static void main(String argv[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
-        System.out.print(1 + " ");
+        
         solve2(n);
 
     }
+    
+    
+    static void solveSeive(int n) {
+        byte[] primes = new byte[n+1];
+        primes[0] = primes[1] = 1;
+        for(int i = 0; i <= (int)Math.sqrt(n); i++) {
 
+            if(primes[i] == 1) continue;
+            for(int j = i*i; j <= n; j+=i) {
+                primes[j] = 1;
+            }
+        }int temp = n;
+        for(int i = 2; i <= n; i++) {
+            if(primes[i] == 1) continue;
+            while(temp%i == 0) {
+                temp/=i;
+                System.out.print(i+"*");
+            }
+        }
+        System.out.println();
+    }
     static void solve2(int n) {
         for (int i = 2; i <= Math.sqrt(n); i++) {
             while (n % i == 0) {
-                System.out.print(i + " ");
+                System.out.print(i + "*");
                 n /= i;
-
             }
         }
         if (n != 1)
